f23a2607dbb26efa5b452ce3b262e6bf0e18639e
SenolIsci <52200971+SenolIsci@users.noreply.github.com>
2021-04-06 18:39:19 +0300

    Update classmethod_example.py

--- classmethod_example.py
+++ classmethod_example.py
@@ -1,3 +1,8 @@
+"""
+Example 2: Create factory method using class method
+"""
+
+
 # Python program to demonstrate 
 # use of a class method and static method.
 from datetime import date
@@ -33,4 +38,42 @@ Output:
 21
 22
 True
+"""
+
+"""
+Example : How the class method works for the inheritance?
+from datetime import date
+"""
+# random Person
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @staticmethod
+    def fromFathersAge(name, fatherAge, fatherPersonAgeDiff):
+        return Person(name, date.today().year - fatherAge + fatherPersonAgeDiff)
+
+    @classmethod
+    def fromBirthYear(cls, name, birthYear):
+        return cls(name, date.today().year - birthYear)
+
+    def display(self):
+        print(self.name + "'s age is: " + str(self.age))
+
+class Man(Person):
+    sex = 'Male'
+
+man = Man.fromBirthYear('John', 1985)
+print(isinstance(man, Man))
+
+man1 = Man.fromFathersAge('John', 1965, 20)
+print(isinstance(man1, Man))
+
+"""
+Output
+
+True
+False
+
 """
